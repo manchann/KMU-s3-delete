af98d09ca583b52e7ce7dd37441fa89300746b94
chl8273@naver.com <chl8273@naver.com>
2020-11-18 16:40:45 +0900

    modify remove

--- task.sh
+++ task.sh
@@ -13,7 +13,7 @@ fi
 
 while read bucket_name; do
 
-  echo -e "버킷 이름: "$bucket_name"\n"
+  echo -e "\n버킷 이름: "$bucket_name"\n"
   echo "버킷 내부 오브젝트 불러오는 중... 오브젝트의 내용이 많다면 시간이 다소 소요될 수 있습니다."
   sleep 1
   aws s3 ls --summarize --human-readable --recursive s3://$bucket_name
@@ -23,12 +23,11 @@ while read bucket_name; do
   read is_delete </dev/tty
 
   if [ $is_delete = yes ]; then
-    aws s3 rb s3://$bucket_name
+    aws s3 rb s3://$bucket_name --force
   elif [ $is_delete = end ]; then
     echo -e "작업을 종료합니다."
     break
   else
-    echo -e "\n"
     continue
   fi
 
