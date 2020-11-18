a925715ed31b9ab413019d01b2bd435973c22549
chl8273@naver.com <chl8273@naver.com>
2020-11-18 16:19:46 +0900

    fin

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # KMU-s3-delete
+
+## AWS s3버킷이 쌓여있을 경우 사용하기 위한 프로그램 입니다.
+
+### 환경 세팅
+AWS CLI를 사용하기 위해 다음과 같는 명령어를 사용하여 계정 로그인을 합니다. 
+```shell script
+asw configure
+```
+
+작업을 시작하기 위해 명령어를 입력합니다.
+```shell script
+source task.sh
+```
+
+modified time이 오래된 순서대로 보려면 command line에 old를 입력하고 그렇지 않을것이라면 old를 제외한 키를 입력합니다.
+
+<img src="../assets/bucket_name.png" />
+
+버킷 이름과 오브젝트의 이름들, 총 개수와, 총 사이즈를 확인합니다.
+
+<img src="../assets/remove_bucket" />
+버킷을 삭제하기 위해 command line에 yes를 입력하고 삭제 메세지를 확인합니다.
+
+삭제하지 않으려면 yes를 제외한 다른 문자를 입력합니다
+
+이를 반복합니다.
+
+작업을 종료하려면 end를 입력하여 종료합니다.
---
+++ task.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+s3_bucket_file="./s3_bucket_list.txt"
+
+echo -e "수정된일이 오래된 순서로 보려면 old를 입력해주세요. 원하시지 않는다면 다른키를 아무커나 입력해주세요"
+read sort_modified_old
+
+if [ $sort_modified_old = old ]; then
+  aws s3 ls / | sort | cut -d' ' -f3 >$s3_bucket_file
+else
+  aws s3 ls / | cut -d' ' -f3 >$s3_bucket_file
+fi
+
+while read bucket_name; do
+
+  echo -e "버킷 이름: "$bucket_name"\n"
+  echo "버킷 내부 오브젝트 불러오는 중... 오브젝트의 내용이 많다면 시간이 다소 소요될 수 있습니다."
+  sleep 1
+  aws s3 ls --summarize --human-readable --recursive s3://$bucket_name
+
+  echo -e "\n"
+  echo -e "버킷을 삭제하시려면 yes, 유지하시려면 다른키를 아무거나 입력해주세요\n종료를 원하신다면 end를 입력해주세요"
+  read is_delete </dev/tty
+
+  if [ $is_delete = yes ]; then
+    aws s3 rb s3://$bucket_name
+  elif [ $is_delete = end ]; then
+    echo -e "작업을 종료합니다."
+    break
+  else
+    echo -e "\n"
+    continue
+  fi
+
+done <$s3_bucket_file
